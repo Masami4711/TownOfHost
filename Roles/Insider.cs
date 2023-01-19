1e3f3a00b1de8a6883980e8f3f9914ea5fde2802
Masami <103577031+Masami4711@users.noreply.github.com>
2023-01-19 11:23:42 +0900

    役職を見る条件の関数を追加

--- Roles/Insider.cs
+++ Roles/Insider.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using UnityEngine;
+using static TownOfHost.Translator;
 using static TownOfHost.Options;
 
 namespace TownOfHost
@@ -28,5 +30,39 @@ namespace TownOfHost
             playerIdList.Add(playerId);
         }
         public static bool IsEnable => playerIdList.Count > 0;
+
+        //基本条件
+        private static bool BasicRequirementToSeeTargetRole(PlayerControl seer, PlayerControl target)
+        {
+            if (seer == null || target == null || !IsEnable) return false;
+            if (seer == target) return false; //自分自身
+            if (!seer.IsAlive() && GhostCanSeeOtherRoles.GetBool()) return false; //幽霊で普通に見えるパターン
+            return playerIdList.Contains(seer.PlayerId);
+        }
+        private static int KillCount(byte playerId)
+            => Main.PlayerStates[playerId].GetKillCount(true);
+
+        //役職を見る条件の関数
+        public static bool KnowTargetRole(PlayerControl seer, PlayerControl target) //Insider能力で役職が分かるケースのみ
+        {
+            if (KnowDeadTargetRole(seer, target)) return true;
+            if (BasicRequirementToSeeTargetRole(seer, target))
+                return target.GetCustomRole().GetRoleType() switch
+                {
+                    RoleType.Impostor
+                        => CanSeeImpostorAbilities.GetBool(),
+                    RoleType.Madmate
+                        => CanSeeMadmates.GetBool() && KillCount(seer.PlayerId) >= KillCountToSeeMadmates.GetInt(),
+                    _ => false,
+                };
+            return false;
+        }
+        public static bool KnowDeadTargetRole(PlayerControl seer, PlayerControl target) //幽霊の役職が見えるケース
+        {
+            if (BasicRequirementToSeeTargetRole(seer, target) && !target.IsAlive())
+                return CanSeeAllGhostsRoles.GetBool() //全員見える
+                    || target.GetRealKiller() == seer; //自分でキルした相手
+            return false;
+        }
     }
 }
